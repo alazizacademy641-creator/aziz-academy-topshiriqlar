b8ca9db3a3277e9eb5e08b20f93b388de7127745
alazizacademy641-creator <alazizacademy641@gmail.com>
2026-07-15 10:15:30 +0500

    Add: O'lchov birliklari konvertori [alazizacademy641-creator]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-O_lchov_birliklari_konvertori.py
@@ -0,0 +1,10 @@
+tur_raqami = int(input())
+qiymat = int(input())
+if tur_raqami == 1:
+    print(qiymat * 1000)
+elif tur_raqami == 2:
+    print(qiymat * 60)
+elif tur_raqami == 3:
+    print(qiymat * 1000)
+else:
+    print("Notogri tanlov")
