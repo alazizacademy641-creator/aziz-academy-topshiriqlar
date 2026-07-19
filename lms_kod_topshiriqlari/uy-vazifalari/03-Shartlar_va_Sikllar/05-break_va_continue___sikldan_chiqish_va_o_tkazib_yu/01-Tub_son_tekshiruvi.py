069057ffa7bca1c68d202a7b1fe17975e0b317ed
alazizacademy641-creator <alazizacademy641@gmail.com>
2026-07-19 21:50:59 +0500

    Add: Tub son tekshiruvi [alazizacademy641-creator]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_son_tekshiruvi.py
@@ -0,0 +1,15 @@
+import math
+n = int(input())
+
+if n <= 1:
+    print("yo'q")
+else:
+    is_prime = True
+    for i in range(2, int(math.isqrt(n)) + 1):
+        if n % i == 0:
+            is_prime= False
+            break
+    if is_prime:
+        print("ha")
+    else:
+        print("yo'q")
